b44b69f52ac453a4594c3897c36e7235c17cb245
ifham baig <ifhambaig@ifham-ka-MacBook-Air.local>
2025-04-14 02:21:50 +0530

    new

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const app = require('./app');
 const connectDB = require('./confiq/Db')
+require('dotenv').config();
 
+const port = process.env.PORT || 3000;
 
 
 connectDB();
 
-// app.listen(3000, () => {
-//     console.log('Server is running on port 3000');
-// }
-// );
+app.listen(port, () => {
+    console.log('Server is running on port 3000');
+}
+);
